e22f7e32138a2f1abcf95b365a8b863359ddeb37
nuwanda <none@none.com>
2009-03-09 19:20:10 +0000

    Enh: correct version information

--- Main/Exceptional/Properties/AssemblyInfo.cs
+++ Main/Exceptional/Properties/AssemblyInfo.cs
@@ -1,13 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using JetBrains.UI.Shell.PluginSupport;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Exceptional plugin for ReSharper")]
-[assembly: AssemblyDescription("Analyses exceptions usage and suggests improvements.")]
+[assembly: AssemblyTitle("Exceptional plug-in for ReSharper")]
+[assembly: AssemblyDescription("Analyzes exceptions usage and suggests improvements.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodeGears")]
 [assembly: AssemblyProduct("Exceptional")]
@@ -16,9 +15,9 @@ using JetBrains.UI.Shell.PluginSupport;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3628d589-e118-4c2c-bd8e-fdef6b6ed07c")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: PluginTitle("Exceptional")]
-[assembly: PluginDescription("Analyses exceptions usage and suggests improvements.")]
+[assembly: PluginDescription("Analyzes exceptions usage and suggests improvements.")]
 [assembly: PluginVendor("CodeGears")]
