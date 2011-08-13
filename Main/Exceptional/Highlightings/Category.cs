0bb8cec09e6bf3b23eb21c932c194e0086b40520
bleedo <none@none.com>
2011-08-13 15:21:04 +0000

    New file Highlightings\Category.cs for new parameter to StaticSeverityHighlightingAttribute in 6.0

---
+++ Main/Exceptional/Highlightings/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeGears.ReSharper.Exceptional.Highlightings
+{
+	class Category
+	{
+		public const string Title = "ErrorHandling";
+	}
+}
