de0b502c995d36800634b538f4302e08839deda1
Rico Suter <mail@rsuter.com>
2014-12-26 16:09:05 +0100

    Added new issue class

---
+++ Exceptional.Playground/Issues/DelegateIssues.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Security;
+
+namespace Exceptional.Playground.Issues
+{
+    class DelegateIssues
+    {
+        public void Foo1()
+        {
+            Action x = delegate
+            {
+                throw new SecurityException(); // Optional warning
+            };
+
+            Action y = delegate
+            {
+                var a = 10;
+                Bar(); // Optional warning
+            };
+
+            Action z = delegate
+            {
+                var b = new DelegateIssues(); // Issue: Should be optional warning  
+            };
+        }
+
+        public void Foo2()
+        {
+            Action x = () =>
+            {
+                throw new SecurityException(); // Issue: Should be optional warning 
+            };
+
+            Action y = () =>
+            {
+                var a = 10;
+                Bar(); // // Issue: Optional warning
+            };
+
+            Action z = () =>
+            {
+                var b = new DelegateIssues(); // Issue: Should be optional warning 
+            };
+        }
+
+        public void Foo3()
+        {
+            Action y = Bar; // No warning
+        }
+
+        /// <exception cref="SecurityException">Condition. </exception>
+        public void Bar()
+        {
+            throw new SecurityException();
+        }
+
+        /// <exception cref="SecurityException">Condition. </exception>
+        public DelegateIssues()
+        {
+            throw new SecurityException();
+        }
+    }
+}
