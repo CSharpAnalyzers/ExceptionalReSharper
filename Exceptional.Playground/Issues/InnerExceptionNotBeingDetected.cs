b2ced2022b65c03e0f8ca7649b7da9b2622c3bd5
Rico Suter <mail@rsuter.com>
2015-02-16 21:00:46 +0100

    Added InnerExceptionNotBeingDetected

---
+++ Exceptional.Playground/Issues/InnerExceptionNotBeingDetected.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exceptional.Playground.Issues
+{
+    class InnerExceptionNotBeingDetected
+    {
+        /// <exception cref="ArgumentException">Item could not be found.</exception>
+        public void Foo(string item)
+        {
+            try
+            {
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                throw new ArgumentException("Item could not be found.", "item", ex); // Issue: Include caught exception is not working
+            }
+        }
+    }
+}
