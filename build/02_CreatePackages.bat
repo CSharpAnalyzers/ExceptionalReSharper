832ce89acec832d33522683da4fd2e1303589b37
Ewen @ HQ027026 <Ewen@CADbloke.com>
2016-04-27 09:53:41 +1000

    Whups, added that for testing. Sorry.

--- build/02_CreatePackages.bat
+++ build/02_CreatePackages.bat
@@ -2,4 +2,3 @@ nuget.exe pack Exceptional.R8.nuspec -OutputDirectory "Packages"
 nuget.exe pack Exceptional.R9.nuspec -OutputDirectory "Packages"
 nuget.exe pack Exceptional.R10.nuspec -OutputDirectory "Packages"
 nuget.exe pack Exceptional.2016.1.nuspec -OutputDirectory "Packages"
-pause
