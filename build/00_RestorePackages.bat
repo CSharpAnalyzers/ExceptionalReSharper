59fa974c491d168cee9b69c5c6067c08a75b4d11
Ewen @ CADbloke <Ewen@CADbloke.com>
2016-04-27 09:46:11 +1000

    v2016.1: copied v10 csproj, updated Nugets & packaging .bat files. ...
    Things I didn't do ...
    03_PushPackages.bat needs updating.
    Didn't change ANY code.
    
    Tested in VS2015.2 with R# 2016.1 RTM and it seems to work ok.

--- build/00_RestorePackages.bat
+++ build/00_RestorePackages.bat
@@ -4,3 +4,4 @@ msbuild ../src/Exceptional.sln /p:Configuration=Release /t:rebuild
 nuget.exe pack Exceptional.R8.nuspec -OutputDirectory "Packages"
 nuget.exe pack Exceptional.R9.nuspec -OutputDirectory "Packages"
 nuget.exe pack Exceptional.R10.nuspec -OutputDirectory "Packages"
+nuget.exe pack Exceptional.2016.1.nuspec -OutputDirectory "Packages"
