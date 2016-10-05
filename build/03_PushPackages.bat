847c2525729fcccc1ab55a87347e06ea10c788cc
Rico Suter <mail@rsuter.com>
2016-10-05 17:51:05 +0200

    v0.7.12.0

--- build/03_PushPackages.bat
+++ build/03_PushPackages.bat
@@ -1,8 +1,8 @@
 set /p apiKey=NuGet API Key: 
 set /p version=Package Version: 
 
-nuget.exe push Packages/Exceptional.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v5.0/
-nuget.exe push Packages/Exceptional.R9.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v5.0/
-nuget.exe push Packages/Exceptional.R10.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v5.0/
-nuget.exe push Packages/Exceptional.2016.1.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v5.0/
-nuget.exe push Packages/Exceptional.2016.2.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v5.0/
+nuget.exe push Packages/Exceptional.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com
+nuget.exe push Packages/Exceptional.R9.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com
+nuget.exe push Packages/Exceptional.R10.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com
+nuget.exe push Packages/Exceptional.2016.1.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com
+nuget.exe push Packages/Exceptional.2016.2.%version%.nupkg %apiKey% -source https://resharper-plugins.jetbrains.com
