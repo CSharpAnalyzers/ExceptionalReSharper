db459145238ed525d67a3a6c7eb8702c48b61f47
Pedro Magueija <pedromagueija@gmail.com>
2016-10-14 19:51:50 +0200

    Fix #27
    Introduces support for nameof with ArgumentNullException.

---
+++ src/Exceptional.Playground/Fixed/NameOfSupportedWithArgumentNullException.cs
@@ -0,0 +1,49 @@
+﻿namespace Exceptional.Playground.Fixed
+{
+    using System;
+
+    internal class NameOfSupportedWithArgumentNullException
+    {
+        public void Foo(object bar)
+        {
+            if (bar == null)
+            {
+                // Alt+Enter -> Add documentation 
+                // should generate correct paramref name "bar"
+                throw new ArgumentNullException(nameof(bar));                    
+            }
+        }
+
+        public void Foo2(object bar)
+        {
+            if (bar == null)
+            {
+                throw new ArgumentNullException("some neat message", (Exception)null);
+            }
+        }
+
+        public void Foo3(object bar)
+        {
+            if (bar == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public void Foo4(object bar)
+        {
+            if (bar == null)
+            {
+                throw new ArgumentNullException("bar", "message");
+            }
+        }
+
+        public void Foo5(object bar)
+        {
+            if (bar == null)
+            {
+                throw new ArgumentNullException(nameof(bar), "message");
+            }
+        }
+    }
+}
