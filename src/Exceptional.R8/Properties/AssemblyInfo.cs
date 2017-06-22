b5a601823442afc7769a47e45f3dfc0364617dbb
Rico Suter <mail@rsuter.com>
2017-06-22 08:28:54 +0200

    v0.9.0

--- src/Exceptional.R8/Properties/AssemblyInfo.cs
+++ src/Exceptional.R8/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyDescription("Analyzes thrown and documented exceptions and suggests improvements. ")]
 [assembly: AssemblyProduct("Exceptional")]
 [assembly: AssemblyCompany("ExceptionalDevs")]
-[assembly: AssemblyCopyright("Copyright © 2015 ExceptionalDevs")]
+[assembly: AssemblyCopyright("Copyright ï¿½ 2015 ExceptionalDevs")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.7.13.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3628d589-e118-4c2c-bd8e-fdef6b6ed07c")]
 
