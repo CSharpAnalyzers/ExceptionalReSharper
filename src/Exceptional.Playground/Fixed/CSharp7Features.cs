e2afb6f3f5578d6954e8a46eaca9920e0ddf0a97
Rico Suter <mail@rsuter.com>
2017-07-18 12:58:38 +0200

    Added CSharp7Features

---
+++ src/Exceptional.Playground/Fixed/CSharp7Features.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Exceptional.Playground.Fixed
+{
+    public class CSharp7Features : INotifyPropertyChanged
+    {
+        private string _property1;
+        private string _property2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Property1
+        {
+            get => this._property1;
+            set => this._property1 = value ?? throw new NullReferenceException("Foo bar"); // Warning: Should insert with "set"
+        }
+
+        public string Property2
+        {
+            get => this._property2;
+
+            set
+            {
+                this._property2 = value;
+                this.OnPropertyChanged(); // Warning
+            }
+        }
+
+        /// <exception cref="Exception" accessor="set">A delegate callback throws an exception.</exception>
+        public string Property3
+        {
+            get => this._property2;
+            set
+            {
+                this._property2 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
+        public void Method1()
+        {
+            this.Property1 = this.Property3; // No warning
+        }
+
+        /// <exception cref="Exception">A delegate callback throws an exception.</exception>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void OnPropertyChanged2([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); // Warning
+        }
+    }
+}
