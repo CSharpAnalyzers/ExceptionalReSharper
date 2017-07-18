23e70975cdfcefd220f271cd90c7e55bc2219687
Rico Suter <mail@rsuter.com>
2017-07-18 12:50:23 +0200

    v0.10.0.0

---
+++ src/Exceptional.Playground/Fixed/DisableWarnings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exceptional.Playground.Fixed
+{
+    public class DisableWarnings
+    {
+        public void Foo()
+        {
+            // ReSharper disable once ExceptionNotDocumentedOptional
+            throw new NotImplementedException("Bar"); // no warning
+            // ReSharper restore once ExceptionNotDocumentedOptional
+            throw new NotImplementedException("Bar"); // warning
+        }
+    }
+}
