28401b7bbe6344333bcb93cbb434c1764e6abe61
Darius Weber <darius.weber@posteo.de>
2020-02-23 10:30:56 +0100

    Stop failing GitHub Action nightly build if no changes were detected

--- build/precondition.ps1
+++ build/precondition.ps1
@@ -1,21 +1,13 @@
-$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
-$NightlyRevFile = Join-Path -Path $ScriptDir -ChildPath nightly.rev
+$scriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$nightlyRevFile = Join-Path -Path $scriptDir -ChildPath nightly.rev
 
-$PreviousHash = git rev-parse HEAD~1
-$HashOfLatestBuild = Get-Content -Path $NightlyRevFile -ErrorAction SilentlyContinue
+$previousHash = git rev-parse HEAD~1
+$hashOfLatestBuild = Get-Content -Path $nightlyRevFile -ErrorAction SilentlyContinue
 
-Write-Output "Previous hash:     $PreviousHash"
-Write-Output "Latest Build Hash: $HashOfLatestBuild"
+$hasChanges = -Not ($previousHash -eq $hashOfLatestBuild)
 
-$HasChanges = -Not ($PreviousHash -eq $HashOfLatestBuild)
-
-if ($HasChanges -eq $true)
-{
-    "Some changes detected"
-    exit 0
-}
-else
-{
-    Write-Output "No changes detected"
-    exit 1
+return @{
+    "LatestBuildHash" = $hashOfLatestBuild;
+    "PreviousHash" = $previousHash;
+    "HasChanges" = $hasChanges;
 }
